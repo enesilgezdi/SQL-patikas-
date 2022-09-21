6e5e9a55776820e00af40ce01eaa024b293ecb84
enes ilgezdi <105016431+enesilgezdi@users.noreply.github.com>
2022-09-21 15:28:06 +0300

    Update odev2-2.sql

--- odev2-2.sql
+++ odev2-2.sql
@@ -1,7 +1,7 @@
 -- 1 SELECT * FROM film
 --   WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 
--- 2 SELECT * FROM actor
+-- 2 SELECT first_name, last_name FROM actor
 --   WHERE firt_name IN('Penelope' , 'Nick', 'Ed');
 
 -- 3 SELECT * FROM film
